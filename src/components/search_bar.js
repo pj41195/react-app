a5049fc302b6766b9a77788874b66577bd5d8640
Praval Jain <praval41195jain@gmail.com>
2018-08-16 16:58:38 +0530

    complete youtube app upto module two

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,19 +3,22 @@ import React, {Component} from 'react';
 class SearchBar extends Component {
     constructor(props) {
        super(props);
-       this.state = {term: 'test'};
+       this.state = {term: ''};
     }
 
     render() {
 
        return (
-           <div>
+           <div className="search-bar">
              <input 
              value = {this.state.term}
-             onChange = {event => this.setState({ term : event.target.value })} />
-             Value of the input : {this.state.term}
+             onChange = {event => this.onInputChange(event.target.value)} />
            </div>
        );
+    }
+    onInputChange(term) {
+       this.setState({term});
+       this.props.onsearchTermChange(term);
     }
 }
 export default SearchBar;
